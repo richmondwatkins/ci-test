46f40cd600e455b5a8ab1298a0410b8283167541
richmondwatkins <richmondwatkins@mac.com>
2015-01-24 20:12:33 -0600

    adding fonts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+(function(){
+
+	$(document).ready(function(){
+    checkScrollPosition();
+		$(window).scroll(checkScrollPosition); 
+	});
+
+  function checkScrollPosition(){
+
+      if($(window).scrollTop() >= 418){
+
+        $('#subscribe').hide();
+        $('#scroll-search').show();
+      }else {
+        $('#subscribe').show();
+        $('#scroll-search').hide();
+      }
+    }
+
+})();
 
